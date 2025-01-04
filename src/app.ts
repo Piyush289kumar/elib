5a80e23c5d90677e1646695fa87091301fdcfea4
piyush289kumar <piyushraikwar289@gmail.com>
2025-01-04 20:52:22 +0530

    Add Global Error Handler Middleware

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,19 @@
 import express, { NextFunction, Request, Response } from "express";
-import { HttpError } from "http-errors";
+import createHttpError, { HttpError } from "http-errors";
 import { config } from "./config/config";
+import globalErrorHandler from "./middlewares/globalErrorHandler";
 
 const app = express();
 
 // Rotues
 // Https Method -> GET, POST, PUT, PATCH, DELETE
 app.get("/", (req: Request, res, next) => {
+  const error = createHttpError(404, "Something went wrong...");
+  throw error;
   res.json({ message: "Server is Running.." });
 });
 
 // Global Error Handler
-app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
-  const statusCode = err.status || 500;
-
-  res.status(statusCode).json({
-    message: err.message,
-    errorStack: config.node_env === "Local" ? err.stack : "",
-  });
-});
+app.use(globalErrorHandler);
 
 export default app;
