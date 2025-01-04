78d4e13cbd73eb5b40f2d839c28613f514654c1e
piyush289kumar <piyushraikwar289@gmail.com>
2025-01-04 21:14:13 +0530

    Create User Router & Controller

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,17 @@
-import express, { NextFunction, Request, Response } from "express";
-import createHttpError, { HttpError } from "http-errors";
-import { config } from "./config/config";
+import express from "express";
 import globalErrorHandler from "./middlewares/globalErrorHandler";
+import userRouter from "./users/userRoutes";
 
 const app = express();
 
 // Rotues
+
 // Https Method -> GET, POST, PUT, PATCH, DELETE
-app.get("/", (req: Request, res, next) => {
-  const error = createHttpError(404, "Something went wrong...");
-  throw error;
+app.get("/", (req, res) => {
   res.json({ message: "Server is Running.." });
 });
+
+app.use("/api/users", userRouter);
 
 // Global Error Handler
 app.use(globalErrorHandler);
